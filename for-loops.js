520adbfabc0fe825882e7dffb4cfea69d29e6605
Mark Ellis <markellisdev@gmail.com>
2016-10-14 10:25:35 -0500

    for loop division and presidents array

--- for-loops.js
+++ for-loops.js
@@ -1,7 +1,17 @@
 
 
-for (var i = 5; i < 121; i += 5) {
+// for (var i = 5; i < 121; i += 5) {
 	
-	console.log(i);
+// 	console.log(i);
 
+// }
+
+// for (var i = 4096; i >= 1; i /= 2) {
+// 	console.log(("I = ") + i);
+// }
+
+var presidents = ["George Washington", "John Adams", "Thomas Jefferson", "James Madison", "James Monroe", "John Quincy Adams", "Andrew Jackson", "Martin Van Buren", "William Henry Harrison", "John Tyler"];
+
+for (var i = 0; i <= presidents.length; i++) {
+	console.log(("President #") + (i + 1) + (" was ") + presidents[i]);
 }

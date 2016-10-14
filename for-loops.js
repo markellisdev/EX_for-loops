4cb27a3a0ae5524307db7925526fd49494287f92
Mark Ellis <markellisdev@gmail.com>
2016-10-14 10:51:13 -0500

    for in iteration

--- for-loops.js
+++ for-loops.js
@@ -12,6 +12,24 @@
 
 var presidents = ["George Washington", "John Adams", "Thomas Jefferson", "James Madison", "James Monroe", "John Quincy Adams", "Andrew Jackson", "Martin Van Buren", "William Henry Harrison", "John Tyler"];
 
-for (var i = 0; i <= presidents.length; i++) {
-	console.log(("President #") + (i + 1) + (" was ") + presidents[i]);
+for (var i = 0; i < presidents.length; i++) {
+	console.log("President #" + (i + 1) + " was " + presidents[i]);
+}
+
+var antSpecies = {
+  argentine: {},
+  army: {},
+  bigHeaded: {},
+  black: {},
+  bull: {},
+  carpenter: {},
+  crazy: {},
+  fire: {},
+  glider: {},
+  honeyPot: {},
+  jackJumper: {}
+}
+
+for (var key in antSpecies) {
+	console.log("Key is " + key);
 }
